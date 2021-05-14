57336609967b88116f3a7ef0aa58f8c3a5e8542e
megan <megan.schertz@gmail.com>
2021-05-14 13:21:03 -0500

    commit tree f0713e6bd116b4d0b104bd2073373994444b758f

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    const match = drivers.filter( driver => {
+        return driver.toUpperCase() == string.toUpperCase()
+    })
+    return match
+}
+
+function fuzzyMatch(drivers, string) {
+    const match = drivers.filter(driver => {
+        return driver.slice(0, 2) === string.slice(0, 2)
+    })
+    return match
+}
+
+function matchName(drivers, string) {
+    const match = drivers.filter(driver => {
+        return driver["name"] === string
+    })
+    return match
+}
